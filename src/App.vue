bfea63a3fe26decd96ca07ad46fc33a2886703c8
Gaël <gael.bachelier@gmail.com>
2022-12-22 18:44:34 +0100

    card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="container">
+  <!-- <div class="container">
+    <img :src="require('./assets/bg.jpg')">
+    <div class="hidder">
+      <vue-scratchable v-slot="{ init }" :brushOptions="brush" :hideOptions="hide" :onClick="startConfetti" :onRelease="stopConfetti">
+        <img :src="require('./assets/bg.jpg')" @load="init">
+      </vue-scratchable>
+    </div>
+  </div> -->
+  <div class="card">
     <img :src="require('./assets/bg.jpg')">
     <div class="hidder">
       <vue-scratchable v-slot="{ init }" :brushOptions="brush" :hideOptions="hide" :onClick="startConfetti" :onRelease="stopConfetti">
@@ -11,6 +19,8 @@
 
 <script>
 import VueScratchable from './components/vue-scratchable.vue';
+
+VueScratchable.__defaults;
 
 export default {
   components: {
@@ -64,15 +74,59 @@ body {
   height: 100%;
   margin: 0;
   overflow: hidden;
+  background-color: #aaaaaa;
 }
 
 #app {
   width: 100%;
   height: 100%;
   margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.container {
+.card{
+  background-color: white;
+  border-radius: 1.5rem;
+  box-shadow: 10px 10px 5px rgb(0.6, 0.6, 0.6);
+  width: 90%;
+  aspect-ratio: 16/9;
+  overflow: hidden;
+  position: relative;
+}
+
+.card > img {
+  width: 100%;
+  object-fit: contain;
+  -o-object-fit: contain;
+  object-position: center;
+  -o-object-position: top;
+  position: absolute;
+}
+
+.hidder {
+  position: absolute;
+  display: block;
+  width: 20%;
+  height: 11%;
+  /* background: rgba(0.9, 0.9, 0.9, 0.5); */
+  transform: translate(-50%, -50%);
+  top: 15.5%;
+  left: 19%;
+}
+
+.vue-scratchable-wrapper {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+
+.vue-scratchable-wrapper > img {
+  opacity: 0;
+}
+
+/* .container {
   position: relative;
 }
 
@@ -105,5 +159,5 @@ body {
 
 .vue-scratchable-wrapper > img {
   opacity: 0;
-}
+} */
 </style>
